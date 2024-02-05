c8647f1035d74a1f7dae1e215c019f88a352eb66
ArchiChambial232 <159005877+ArchiChambial232@users.noreply.github.com>
2024-02-05 14:42:14 +0530

    Add files via upload
    First Commit Rectangle Class Added

---
+++ PROG2070_Console_App/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070_Console_App
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        // Default constructor
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        // Non-default constructor
+        public Rectangle(int initialLength, int initialWidth)
+        {
+            length = initialLength;
+            width = initialWidth;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public void SetLength(int newLength)
+        {
+            length = newLength;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public void SetWidth(int newWidth)
+        {
+            width = newWidth;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
