1aadfb1ee543371b836f0adc39744bdd44a5400f
ArchiChambial232 <159005877+ArchiChambial232@users.noreply.github.com>
2024-02-05 16:55:28 +0530

    Add files via upload
    Second Commit RectangleTests Class  and test all unit test case

---
+++ PROG2070_Console_App/PROG2070_Console_App/RectangleTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070_Console_App
+{
+    [TestClass]
+    public class RectangleTests
+    {
+        [TestMethod]
+        public void GetLength_ShouldReturnDefaultLength()
+        {
+            // Arrange
+            Rectangle rectangle = new Rectangle();
+
+            // Act
+            int result = rectangle.GetLength();
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+       
+    }
+}
